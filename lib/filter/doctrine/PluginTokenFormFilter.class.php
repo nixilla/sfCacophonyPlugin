d0b05482fcc3bb192090f1408c7030f5c3fce7fc
Janusz Slota <janusz.slota@nixilla.com>
2011-09-20 17:03:45 +0100

    - added first draft for the token table

---
+++ lib/filter/doctrine/PluginTokenFormFilter.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PluginToken form.
+ *
+ * @package    sfCacophonyPlugin
+ * @subpackage filter
+ * @author     Janusz Slota <janusz.slota@nixilla.com>
+ */
+abstract class PluginTokenFormFilter extends BaseTokenFormFilter
+{
+  
+}
---
+++ lib/form/doctrine/PluginTokenForm.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * PluginToken form.
+ *
+ * @package    sfCacophonyPlugin
+ * @subpackage form
+ * @author     Janusz Slota <janusz.slota@nixilla.com>
+ */
+abstract class PluginTokenForm extends BaseTokenForm
+{
+}
