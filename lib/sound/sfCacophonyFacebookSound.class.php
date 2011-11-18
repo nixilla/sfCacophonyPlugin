7ca40ed8196dc0f2c115a260e8b4b351595c65ee
Janusz Slota <janusz.slota@nixilla.com>
2011-11-18 14:37:24 +0000

    - added isset to prevent notice message

--- lib/sound/sfCacophonyFacebookSound.class.php
+++ lib/sound/sfCacophonyFacebookSound.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * 
+ *
  * @package    sfCacophonyPlugin
  * @subpackage sfCacophonyFacebookSound
  * @author     Janusz Slota <janusz.slota@nixilla.com>
@@ -10,30 +10,30 @@ class sfCacophonyFacebookSound
 {
   /**
    * Calls Facebook me method
-   * 
+   *
    * @param array $accessToken
-   * @return array 
+   * @return array
    */
   public static function getMe($accessToken)
   {
     $graph_url = sprintf('https://graph.facebook.com/me?access_token=%s',$accessToken['access_token']);
     $tmp = json_decode(file_get_contents($graph_url));
-    
+
     $user['normalized']['providers_user_id'] = $tmp->id;
     $user['normalized']['username'] = $tmp->username;
     $user['normalized']['first_name'] = $tmp->first_name;
     $user['normalized']['last_name'] = $tmp->last_name;
-    if($tmp->email) $user['normalized']['email_address'] = $tmp->email;
-    
+    if(isset($tmp->email)) $user['normalized']['email_address'] = $tmp->email;
+
     $user['raw'] = $tmp;
-    
+
     return $user;
-    
+
   }
-  
+
   /**
    * Calls Facebook graph methods
-   * 
+   *
    * @param string $method
    * @param array $accessToken
    * @param null $oauth - not used
@@ -43,11 +43,11 @@ class sfCacophonyFacebookSound
   public static function call($method, $accessToken = null, $oauth = null, $params = array())
   {
     $resource = sprintf('%s/%s?', 'https://graph.facebook.com', $method);
-    
+
     if($accessToken) $resource = sprintf('%s&%s', $resource, http_build_query(array('access_token' => $accessToken['access_token'])));
-    
+
     if(count($params)) $resource = sprintf('%s&%s', $resource, http_build_query($params));
-    
+
     if($oauth->fetch($resource))
       return $oauth->getLastResponse();
   }
