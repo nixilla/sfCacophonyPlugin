e45f4131d01159d1924166a2b7a4e1da633a0f6e
Janusz Slota <janusz.slota@nixilla.com>
2012-01-11 16:17:35 +0000

    Added support for Netflix

--- modules/sfCacophonyConsumer/lib/BasesfCacophonyConsumerActions.class.php
+++ modules/sfCacophonyConsumer/lib/BasesfCacophonyConsumerActions.class.php
@@ -17,86 +17,39 @@ class BasesfCacophonyConsumerActions extends sfActions
   public function executeConnect(sfRequest $request)
   {
     $this->forward404Unless($request->getParameter('provider'));
-    
-    // facebook uses OAuth 2.0
-    if($request->getParameter('provider') == 'facebook')
-      $this->forward($request->getParameter('module'), 'facebook');
-    
+
     $config = sfConfig::get('app_cacophony');
     $this->forward404Unless(in_array($request->getParameter('provider'), array_keys($config['providers'])));
-    
+
     if( ! $this->getUser()->isAuthenticated())
-    {
-      $result = sfCacophonyOAuth::getRequestToken($request->getParameter('provider'));
-      
-      $this->getUser()->setAttribute('requestToken',$result,sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')));
-      
-      $this->redirect(
-        sprintf(
-          '%s?%s',
-          $config['providers'][$request->getParameter('provider')]['authorize_url'],
-          http_build_query(array('oauth_token' => $result['oauth_token']))
-        )
-      );
-    }
-    else $this->redirect('@homepage');
-  }
-  
-  /**
-   * Processes the callback from OAuth provider
-   * 
-   * @param sfRequest $request 
-   */
-  public function executeCallback(sfRequest $request)
-  {
-    $this->forward404Unless($request->getParameter('provider'));
-    
-    if($request->getParameter('provider') == 'facebook')
-      $this->forward($request->getParameter('module'), 'facebookcallback');
-    
-    $config = sfConfig::get('app_cacophony');
-    $this->forward404Unless(in_array($request->getParameter('provider'), array_keys($config['providers'])));
-    
-    if( ! $this->getUser()->isAuthenticated())
-    {
-      $requestToken = $this->getUser()->getAttribute('requestToken',null,sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')));
-      
-      if($requestToken)
-      {
-        try
-        {
-          $this->getUser()->setAttribute(
-            'accessToken',
-            sfCacophonyOAuth::getAccessToken(
-              $request->getParameter('provider'),
-              $request->getParameter('oauth_token'),
-              $requestToken['oauth_token_secret'],
-              $request->getParameter('oauth_verifier')
-            ),
-            sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider'))
-          );
+      call_user_func(array($this,sprintf('%sConnect', $request->getParameter('provider'))), $request);
+    else
+      $this->redirect('@homepage');
 
-          $this->getUser()->getAttributeHolder()->remove('requestToken',null,sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')));
-
-          // add me to session
-          $me = sfCacophonyOAuth::getMe(
-              $request->getParameter('provider'),
-              $this->getUser()->getAttribute('accessToken',null,sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')))
-            );
-
-          $this->getUser()->setAttribute('me',$me['normalized'],sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')));
-        }
-        catch(Exception $e)
-        {
-          $this->getUser()->setFlash('error', sprintf('Failed to retrieve access token: %s',$e->getMessage()));
-          $this->redirect('@homepage');
-        }
-      }
-    }
-    else $this->redirect('@homepage');
-    
     return sfView::NONE;
   }
+
+  /**
+   * Executes callback action
+   *
+   * @param sfWebRequest $request
+   */
+  public function executeCallback(sfWebRequest $request)
+  {
+    $this->forward404Unless($request->getParameter('provider'));
+
+    $config = sfConfig::get('app_cacophony');
+    $this->forward404Unless(in_array($request->getParameter('provider'), array_keys($config['providers'])));
+
+    if( ! $this->getUser()->isAuthenticated())
+      call_user_func(array($this,sprintf('%sCallback', $request->getParameter('provider'))), $request);
+    else
+      $this->redirect('@homepage');
+
+    return sfView::NONE;
+  }
+  
+
   
   /**
    * This is only example of what you can do.
@@ -168,9 +121,36 @@ class BasesfCacophonyConsumerActions extends sfActions
      */
     $this->redirect('@homepage');
   }
-  
-  
-  public function executeFacebook(sfWebRequest $request)
+
+  /**
+   * Default connect method, usually called by __call
+   *
+   * @param $request
+   */
+  public function defaultConnect($request)
+  {
+    $config = sfConfig::get('app_cacophony');
+    $this->forward404Unless(in_array($request->getParameter('provider'), array_keys($config['providers'])));
+
+    $result = sfCacophonyOAuth::getRequestToken($request->getParameter('provider'));
+
+    $this->getUser()->setAttribute('requestToken',$result,sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')));
+
+    $this->redirect(
+      sprintf(
+        '%s?%s',
+        $config['providers'][$request->getParameter('provider')]['authorize_url'],
+        http_build_query(array('oauth_token' => $result['oauth_token']))
+      )
+    );
+  }
+
+  /**
+   * Facebook connect
+   *
+   * @param sfWebRequest $request
+   */
+  protected function facebookConnect(sfWebRequest $request)
   {
     $this->getUser()->setAttribute('state', md5(uniqid(rand(), true)) , sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')));
     
@@ -193,10 +173,94 @@ class BasesfCacophonyConsumerActions extends sfActions
       )
     );
   }
-  
-  public function executeFacebookcallback(sfWebRequest $request)
+
+  /**
+   * Netflix connect
+   *
+   * @param $request
+   */
+  protected function netflixConnect($request)
   {
-    // CSFR protection as adviced on the 
+    $config = sfConfig::get('app_cacophony');
+    $result = sfCacophonyOAuth::getRequestToken($request->getParameter('provider'));
+
+    $this->getUser()->setAttribute('requestToken',$result,sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')));
+
+    $this->redirect(
+      sprintf(
+        '%s&%s',
+        $result['login_url'], // access token is already added to login_url
+        http_build_query(
+          array(
+            'oauth_consumer_key' => $config['providers'][$request->getParameter('provider')]['consumer_key'],
+            'application_name' => $result['application_name'],
+            'oauth_callback'  => $this->getContext()->getRouting()->hasRouteName('sf_cacophony_callback') ? url_for(sprintf('@sf_cacophony_callback?provider=%s',$request->getParameter('provider')),true) : 'oob'
+          )
+        )
+      )
+    );
+  }
+
+  /**
+   * Processes the callback from OAuth provider, usually called by __call
+   *
+   * @param sfRequest $request
+   * @return sfView::NONE
+   */
+  protected function defaultCallback(sfRequest $request)
+  {
+    $config = sfConfig::get('app_cacophony');
+    $this->forward404Unless(in_array($request->getParameter('provider'), array_keys($config['providers'])));
+
+    if( ! $this->getUser()->isAuthenticated())
+    {
+      $requestToken = $this->getUser()->getAttribute('requestToken',null,sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')));
+
+      if($requestToken)
+      {
+        try
+        {
+          $this->getUser()->setAttribute(
+            'accessToken',
+            sfCacophonyOAuth::getAccessToken(
+              $request->getParameter('provider'),
+              $request->getParameter('oauth_token'),
+              $requestToken['oauth_token_secret'],
+              $request->getParameter('oauth_verifier')
+            ),
+            sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider'))
+          );
+
+          $this->getUser()->getAttributeHolder()->remove('requestToken',null,sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')));
+
+          // add me to session
+          $me = sfCacophonyOAuth::getMe(
+            $request->getParameter('provider'),
+            $this->getUser()->getAttribute('accessToken',null,sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')))
+          );
+
+          $this->getUser()->setAttribute('me',$me['normalized'],sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider')));
+        }
+        catch(Exception $e)
+        {
+          $this->getUser()->setFlash('error', sprintf('Failed to retrieve access token: %s',$e->getMessage()));
+          $this->redirect('@homepage');
+        }
+      }
+    }
+    else $this->redirect('@homepage');
+  }
+
+  /**
+   * Facebook callback (OAuth 2)
+   *
+   * @param $request
+   * @return string
+   * @throws Exception
+   */
+  public function facebookCallback($request)
+  {
+    // CSFR protection as adviced on the
     // http://developers.facebook.com/docs/authentication/
     if($request->getParameter('state') != $this->getUser()->getAttribute('state', null , sprintf('sfCacophonyPlugin/%s',$request->getParameter('provider'))))
       throw new Exception('CSRF attack detected');
@@ -226,7 +290,17 @@ class BasesfCacophonyConsumerActions extends sfActions
       }
     }
     else $this->redirect('@homepage');
-    
-    return sfView::NONE;
+  }
+
+  /**
+   * Intercepts all undefined Connect and Callback methods and redirects to default ones.
+   *
+   * @param $method
+   * @param $params
+   */
+  public function __call($method,$params)
+  {
+    if(stripos($method,'connect') !== false) $this->defaultConnect($params[0]);
+    if(stripos($method,'callback') !== false) $this->defaultCallback($params[0]);
   }
 }
